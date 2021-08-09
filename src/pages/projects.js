d8886e0fdf03d35d39b49ea13b2e8dc0d2e7e562
monsieurr <plouftoto@gmail.com>
2021-08-09 20:18:13 +0200

    minor updates

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,7 +9,7 @@ const ProjectPage = () => {
         <Layout>
             <Seo title="Projects" />
             <h2>COMING SOON</h2>
-            <p>I will put in details the projects I have been making during these past years.</p>
+            <p>I will put in details the projects I have been making during this year.</p>
             <p>Feel free to check my Github page linked below to see my past projects in the meantime.</p>
         </Layout>
     )
